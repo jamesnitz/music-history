00fb7409fa0ae2a934d8184f80aca38ad1e92dff
jamesnitz <jamesanitz2@gmail.com>
2020-03-18 16:01:55 -0500

    first commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿Select * from Genre
+
+-- INSERT INTO Artist (ArtistName, YearEstablished) VALUES ('Cohort37', '2020');
+
+INSERT INTO Song(Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId) VALUES ('I want it that way', '233', '1990', '3', '3', '3');
+
+INSERT INTO album(Title, ReleaseDate, AlbumLength, [Label], ArtistId, GenreId) VALUES ('backstreetsback', '1990', '3', '3', '2', '2');
+
+Select * from Artist
